bc537640aa9307389cf0a34e2d8b6b09a89a447f
Mr-Abraham <codingnerd84@gmail.com>
2021-08-16 02:01:36 +0530

    Added the files

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.btn');
+const list = document.querySelector('.list');
+const header = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const serviceinfo = document.querySelector('.serviceinfo');
+
+btn.addEventListener('click',function(){
+    list.classList.toggle("active");
+    one.classList.toggle("active");
+    two.classList.toggle("active");
+    three.classList.toggle("active");
+    header.classList.toggle("active");
+});
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+    var box = document.querySelectorAll('.box');
+    for( var i = 0; i < box.length; i++ ){
+        var windowheight = window.innerHeight;
+        var revealtop = box[i].getBoundingClientRect().top;
+        var revealpoint = 200;
+        if(revealtop < windowheight - revealpoint){
+            box[i].classList.add("active");
+            console.log(revealtop);
+        }
+        else{
+            box[i].classList.remove("active");
+        }
+    }
+}
